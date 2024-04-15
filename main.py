e0d4f6515e8c0f798ef759959a93239670e1e5d2
Boomehx <bobbiakaphoebe@gmail.com>
2024-04-15 20:18:46 +0100

    Added mode methods for most picked ATK & DEF quarters choosen from other districts

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as npy
+
+#df = pd.read_csv("https://docs.google.com/spreadsheets/d/1yiTNYyfjpvOmbg1_YBhTFs4vp-f8Mcc5vBoMf0a2skI/export?format=csv")
+#df
+
+
+
+def main():
+    active = True
+    while (active == True):
+
+        option = input("Please select option: ")
+        print("Option:" + option)
+
+        if option == "Exit":
+            active = False
+
+    return "bleh"
+
+main()
+
