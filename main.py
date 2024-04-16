0c69f616c17faa9b451038a1afe8db44166cd1a9
Boomehx <bobbiakaphoebe@gmail.com>
2024-04-16 20:14:44 +0100

    Added functions to selection menu. Deleted old table.

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import pandas as pd
 import numpy as npy
 
-#df = pd.read_csv("https://docs.google.com/spreadsheets/d/1yiTNYyfjpvOmbg1_YBhTFs4vp-f8Mcc5vBoMf0a2skI/export?format=csv")
-#df
 
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/1yiTNYyfjpvOmbg1_YBhTFs4vp-f8Mcc5vBoMf0a2skI/export?format=csv")
+
+def modeQrtATK(district, df):
+    return (df[(df["distNameATK"] == str(district))]["distQrtATK"].mode().to_string(index = False)).replace('\n','&')
+
+def modeQrtDEF(district, df):
+    return df[(df["distNameDEF"] == str(district))]["distQrtDEF"].mode().to_string(index = False).replace('\n','&')
 
 
 def main():
     active = True
+    district = input("Please select what district you want to spy on: ")
+
+    
+
     while (active == True):
 
         option = input("Please select option: ")
-        print("Option:" + option)
 
-        if option == "Exit":
-            active = False
+        match option:
 
-    return "bleh"
+            case "Exit":
+                active = False
+        
+            #modeQrtATK
+            case "1":
+                print(modeQrtATK(district, df))
+
+            case "2":
+                print(modeQrtDEF(district, df))
+
+
+            case "District":
+                district = input("Please select what district you want to spy on: ")
+            
+            case _:
+                print("Option doesn't exist please select another option!")
+        
+
+    return print("bleh")
 
 main()
 
